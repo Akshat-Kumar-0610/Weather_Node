b375269c2881edf3c5c55263dd1e196ab0ae50a9
Akshat-Kumar-0610 <akshat8810@gmail.com>
2021-07-07 23:12:31 +0530

    added backticks instead of quotes

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const key = '67897c386bad4a7b81634832210607'
 
 const getWeather = (city)=>{
 
-    const url = 'http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}days=1&aqi=yes&alerts=no'
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}days=1&aqi=yes&alerts=no`
     request({url:url},(error , response)=>{
         if(error){
             console.log(chalk.black.bgRed('There was a problem connecting to internet.'))
