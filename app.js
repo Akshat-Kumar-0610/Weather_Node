ea482748fe1dd583b7aea086413134ca37530fb2
Akshat-Kumar-0610 <akshat8810@gmail.com>
2021-07-07 23:06:38 +0530

    'Updates'

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-const request = require('request')
-const yargs = require('yargs')
 const weather = require('./weather.js')
-const url = 'http://api.weatherapi.com/v1/forecast.json?key=fedd9d6a75744f7d97935207210607&days=1&aqi=yes&alerts=no'
+const chalk = require('chalk')
 
-yargs.command({
-    command: 'weather',
-    describe: 'Enter city.',
-    builder: {
-        city: {
-            describe: 'Enter city.',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler(argv){
-        weather.getWeather(argv.city)
-    }
-
-})
-console.log(yargs.argv) 
-
+if(process.argv[2]){
+    weather.getWeather(process.argv[2])
+}else{
+    return console.log(chalk.black.bgRed('Please enter the city oyu want to find weather for.'))
+}
--- weather.js
+++ weather.js
@@ -1,16 +1,14 @@
 const request = require('request')
 const chalk = require('chalk')
-const url = 'http://api.weatherapi.com/v1/forecast.json?key=fedd9d6a75744f7d97935207210607&days=1&aqi=yes&alerts=no'
-const url1='http://api.weatherapi.com/v1'
-key = 'fedd9d6a75744f7d97935207210607'
+const key = '67897c386bad4a7b81634832210607'
+
 const getWeather = (city)=>{
-    request({url:url1, q:'Mumbai', days:'1', key:key},(error , response)=>{
-        console.log(response)
+
+    const url = 'http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}days=1&aqi=yes&alerts=no'
+    request({url:url},(error , response)=>{
         if(error){
-            console.log(response)
             console.log(chalk.black.bgRed('There was a problem connecting to internet.'))
         }else if(JSON.parse(response.body).error){
-            console.log(response)
             console.log(chalk.black.bgRed('No matching location found!'))
         }else{
             const data = JSON.parse(response.body)
